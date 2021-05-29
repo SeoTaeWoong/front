63190f8a4dbe7b676ec871db640c4a418e8f4d59
SeoTaeWoong <xodnd0618@naver.com>
2021-05-29 23:24:03 +0900

    디지몬 정보페이지 UI작업중2 ver 0.2.3.2

---
+++ js/dg_digimon.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+    var advanced_menu_slide = document.querySelector(".advanced_menu_button");
+
+    var advanced_menu_toggle = function(){
+        document.querySelector(".advanced_menu_group").classList.toggle("advanced_menu_group_toggle");
+    }
+
+    console.log(advanced_menu_slide);
+    advanced_menu_slide.addEventListener("click", advanced_menu_toggle);
+
+}
